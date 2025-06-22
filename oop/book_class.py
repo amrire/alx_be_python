fc79f0baf46c26635612aa7714b1030bf0a22201
x86skwizer <yassineamrire@protonmail.com>
2025-06-22 22:14:07 +0100

    oop

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,19 +1,22 @@
+# book_class.py
+
 class Book:
+    """A class representing a book with title, author, and publication year."""
 
     def __init__(self, title, author, year):
-        """Initialize a Book instance with title, author, and year."""
+        """Initialize the Book instance with title, author, and year."""
         self.title = title
         self.author = author
         self.year = year
-        
+
     def __del__(self):
-        """Print message when a Book instance is deleted."""
+        """Destructor that prints a message when a Book instance is deleted."""
         print(f"Deleting {self.title}")
-        
+
     def __str__(self):
-        """Return a user-friendly string representation of the Book."""
+        """Return a string representation of the Book instance."""
         return f"{self.title} by {self.author}, published in {self.year}"
-    
+
     def __repr__(self):
-        """Return a string that can be used to recreate the Book object."""
+        """Return an official string representation of the Book instance."""
         return f"Book('{self.title}', '{self.author}', {self.year})"
